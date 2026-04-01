d84e3143dd4761fc80220d82969c4ae48d3862c7
NehemieTuring <ewanenehemie30@gmail.com>
2026-04-01 03:43:00 +0100

    Finale2

--- dist/assets/graduation-cap-C-7Xnk-K.js
+++ dist/assets/graduation-cap-C-7Xnk-K.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-B1b-txHB.js";/**
+import{c as a}from"./index-DDvJPhph.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,15 +11,13 @@ export default defineConfig({
   server: {
     port: 5172,
     strictPort: true, // empêche Vite de changer automatiquement de port
+    watch: {
+      ignored: ['**/server/**'],
+    },
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    watch: {
-      ignored: ['**/server/**'],
     },
   },
 })
